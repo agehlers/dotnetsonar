4165ba9e8a11b8c3a28fbe73c616f052ab174fb6
Roland Stens <roland.stens@gmail.com>
2018-01-17 15:02:25 -0800

    Update scan.sh

--- sonar-runner/scan.sh
+++ sonar-runner/scan.sh
@@ -1,6 +1,6 @@
 cd ..
 cd Server
-mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=http://sonarqube-gwells-tools.pathfinder.gov.bc.ca /k:HETSSERVER
+mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=http://sonarqube-agehlers-sandbox.pathfinder.gov.bc.ca /k:HETSSERVER
 dotnet restore
 dotnet build
 dotnet test
