815fce795cd4488f0fd6bdbc4ab597a1351dabc5
Roland Stens <roland.stens@gmail.com>
2018-01-17 13:31:32 -0800

    Update scan.sh

--- sonar-runner/scan.sh
+++ sonar-runner/scan.sh
@@ -1,1 +1,5 @@
-
+mono ~/Tools/ScannerMSBuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.login=XYZ /k:HETSSERVER
+dotnet restore
+dotnet build
+dotnet test
+mono ~/Tools/ScannerMSBuild/SonarQube.Scanner.MSBuild.exe end
